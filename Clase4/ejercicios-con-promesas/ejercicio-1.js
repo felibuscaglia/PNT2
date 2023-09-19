5c696c1688bb2b730566a5b66b46c40304244b7c
felibuscaglia <felipebbuscaglia@gmail.com>
2023-09-18 22:49:36 -0300

    Ejercico 2 de promesas

--- Clase4/ejercicios-con-promesas/ejercicio-1.js
+++ Clase4/ejercicios-con-promesas/ejercicio-1.js
@@ -16,6 +16,45 @@ const archivo1 = "Archivo 1";
 const archivo2 = "Archivo 2";
 const archivo3 = "Archivo 3";
 
-descargarArchivo(archivo1)
-  .then(() => descargarArchivo(archivo2))
-  .then(() => descargarArchivo(archivo3));
+const archivos = [archivo1, archivo2, archivo3];
+
+const modosDescarga = {
+  SIMULTANEO: "simultaneo",
+  SECUENCIAL: "secuencial",
+};
+
+const descargaSecuencial = async (archivos) => {
+  try {
+    for (const archivo of archivos) {
+      await descargarArchivo(archivo);
+    }
+
+    console.log("Finalizó la descarga secuencial de archivos.");
+  } catch (err) {
+    console.error("Error descarga secuencial: " + err);
+  }
+};
+
+const descargaEnSimultaneo = (archivos) => {
+  const promesasDeDescarga = archivos.map((archivo) =>
+    descargarArchivo(archivo)
+  );
+
+  Promise.all(promesasDeDescarga)
+    .then(() =>
+      console.log("Todas las descargas fueron realizadas en simultaneo.")
+    )
+    .catch((err) => console.error("Error descarga en simultaneo: " + err));
+};
+
+const descargarArchivos = (modoDescarga) => {
+  if (modoDescarga === modosDescarga.SIMULTANEO) {
+    descargaEnSimultaneo(archivos);
+  } else if (modoDescarga === modosDescarga.SECUENCIAL) {
+    descargaSecuencial(archivos);
+  } else {
+    console.log("Modo de descarga no reconocido");
+  }
+};
+
+descargarArchivos(modosDescarga.SECUENCIAL);
